a9525563762c08ee7156e0f80e41ec89af1a0114
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-21 12:06:52 +0100

    task1

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,7 +12,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Item to Add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
             index = int(input("Index to remove: "))
