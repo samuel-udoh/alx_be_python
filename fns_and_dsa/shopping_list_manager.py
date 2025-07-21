1aba6c57796ef801a02ab76a2c9b890e0ec78215
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-21 12:01:37 +0100

    task1

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,32 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Item to Add: ")
+            shopping_list.append(item)
+        elif choice == '2':
+            index = int(input("Index to remove: "))
+            del shopping_list[index + 1]
+        elif choice == '3':
+            print("#"*30)
+            for id, item in enumerate(shopping_list):
+                print(f"==> {id + 1}: {item}")
+            print("#"*30)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
