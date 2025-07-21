6a253b958a3e8967bd53068976112013874e693b
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-21 11:53:23 +0100

    task0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str) -> float or str:
+def perform_operation(num1, num2, operation):
     """
     Perform basic arithmetic operations on two numbers.
     
