b95a341a499d7cd5d0238b725add46cb0a466856
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-28 19:07:52 +0100

    task0

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,4 +10,4 @@ class BankAccount:
         self.account_balance -= amount
         return True
     def display_balance(self) :
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${float(self.account_balance)}")
