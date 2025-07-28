9507832065a4e37421c1e9fe7383594aeee4bd3e
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-28 18:34:43 +0100

    task3

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,23 +3,29 @@ class Book:
         self.title = title
         self.author = author
         self._is_checked_out = False
+    
+    def return_book(self):
+        self._is_checked_out = False
 
+    def check_out(self):
+        self._is_checked_out = True
+    def is_available(self):
+        return not self._is_checked_out
 class Library:
     def __init__(self):
         self._book = []
-        self._currentBooks = []
     def add_book(self, obj):
         self._book.append(obj)
     def check_out_book(self, title):
         for book in self._book:
             if book.title == title:
-                book._is_checked_out = True
+                book.check_out()
     def list_available_books(self):
         for book in self._book:
-            if not book._is_checked_out:
+            if book.is_available():
                 print(f"{book.title} by {book.author}")
     def return_book(self, title):
         for book in self._book:
             if book.title == title:
-                book._is_checked_out = False
+                book.return_book()
     
