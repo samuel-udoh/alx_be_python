bcc8d7da46bee0577cda55379f5f0a8e77928a9c
samuel-udoh <chukwuemekasam16@gmail.com>
2025-08-04 14:06:08 +0100

    task0 and task1

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, year) -> None:
+        self.title: str = title
+        self.author: str = author
+        self.year: int = year
+    
+    def __del__(self):
+        print(f"Deleting {self.title}")
+    
+    def __str__(self):
+        return f"{self.author} by {self.author}, published in {self.year}"
+    
+    def __repr__(self) -> str:
+        return f"Book('{self.title}', '{self.author}', {self.year})"
---
+++ oop/library_system.py
@@ -0,0 +1,38 @@
+class Book:
+    def __init__(self, title, author) -> None:
+        self.author = author
+        self.title = title
+    def __str__(self) -> str:
+        return f"Book: {self.title} by {self.author}"
+
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+        self.file_size = file_size
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author,  page_count: int):
+        super().__init__(title, author)
+        self.page_count = page_count
+    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book):
+        if not isinstance(book, (Book, EBook, PrintBook)):
+            raise TypeError("Can only add Book, EBook, or PrintBook instances")
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
+        
